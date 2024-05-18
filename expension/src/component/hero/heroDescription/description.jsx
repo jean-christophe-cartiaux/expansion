de1990d6e8b11662cd4a824bf2fe6dc148c7a228
Jean-Christophe Cartiaux <cartiaux.jc@gmail.com>
2024-05-18 20:31:23 +0200

    finish ?

--- expension/src/component/hero/heroDescription/description.jsx
+++ expension/src/component/hero/heroDescription/description.jsx
@@ -5,7 +5,7 @@ const Description = () => {
 
         <div className="hero-description">
             <h2>Nous vous accompagnons dans votre <span>expansion digitale</span></h2>
-            <p>Le programme Digital Booster d’Expansion est l’atout digital des petites, moyennes et grandes entreprises qui souhaitent développer ou confirmer leur présence en ligne.</p>
+            <p>Le programme Digital Booster d’Expansion est <strong>l’atout digital des petites, moyennes et grandes entreprises</strong> qui souhaitent développer ou confirmer leur présence en ligne.</p>
             <button className="orange"><span> <img src={icon}/> </span>Faites le test sans plus tarder</button>
         </div>
 
--- expension/src/component/hero/heroImg/img.jsx
+++ expension/src/component/hero/heroImg/img.jsx
@@ -1,10 +1,14 @@
-
+import image1 from "../../../img/icons.png"
+import image2 from "../../../img/Rocket.png"
+import image3 from "../../../img/X.png"
 
 const Img = () => {
     return (
         <>
             <div className="hero-img">
-
+                <img className="hero-img1"src={image3} alt="Description de l'image"/>
+                <img className="hero-img2"src={image1} alt="Description de l'image"/>
+                <img className="hero-img3"src={image2} alt="Description de l'image"/>
             </div>
         </>
     )
