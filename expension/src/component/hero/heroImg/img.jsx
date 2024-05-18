4b3ac595e2c865c89590008d042cb432c636e5d3
Jean-Christophe Cartiaux <cartiaux.jc@gmail.com>
2024-05-18 19:07:52 +0200

    finish ?

--- expension/src/component/hero/heroImg/img.jsx
+++ expension/src/component/hero/heroImg/img.jsx
@@ -1,10 +1,10 @@
-// import icon from "../../../img/icons.png";
+
 
 const Img = () => {
     return (
         <>
             <div className="hero-img">
-                {/*<img className="jeton" src={icon}/>*/}
+
             </div>
         </>
     )
